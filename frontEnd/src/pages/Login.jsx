2dc5446065b72e8d662d2f5bc4fde6568780f4bd
francugh23 <francojohannes23@gmail.com>
2024-03-13 11:46:06 +0800

    Login Inputs

--- frontEnd/src/pages/Login.jsx
+++ frontEnd/src/pages/Login.jsx
@@ -1,12 +1,42 @@
-import '../index.css'
+import "../index.css";
+import Logo from "../assets/school-logo.png";
 const Login = () => {
- return (
-  <>
-    <div className='bg-dark'>
-      <h1>Login</h1>
-    </div>
-  </>
- )
-}
+  const width = 250;
 
-export default Login
+  return (
+    <>
+      <div className="bg-dark container-fluid" id="login-container">
+        <div className="row">
+          <div className="col-4 bg-light">
+            <div className="d-flex flex-column justify-content-center align-items-center">
+              <img src={Logo} alt="school-logo" width={width} />
+
+              <div class="form-floating mb-3 shadow-sm w-75">
+                <input
+                  type="text"
+                  class="form-control form-control-sm"
+                  id="floatingInput"
+                  placeholder="00-00000"
+                />
+                <label for="floatingInput">ID</label>
+              </div>
+
+              <div class="form-floating mb-3 shadow-sm w-75">
+                <input
+                  type="password"
+                  class="form-control form-control-sm"
+                  id="floatingInput"
+                  placeholder="*******"
+                />
+                <label for="floatingInput">Password</label>
+              </div>
+            </div>
+          </div>
+          <div className="col bg-success opacity-75"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
